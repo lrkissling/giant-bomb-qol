35863c5d1c755d2fb49d72b68e70ca25f8a9ff68
Lowell Kissling <lrkissling@gmail.com>
2017-03-19 05:35:44 -0700

    various code cleanup in video_navigator.js

--- video_navigator.js
+++ video_navigator.js
@@ -1,13 +1,8 @@
 console.log("giant-bomb-qol loading");
 
-// Have to trawl the web page for the video ID. Twitter link should be safe.
-var attr = document.getElementsByClassName('share-twitter')[0]
-                    .getAttribute('data-event-tracking').split('|');
-var current_video_id = attr[attr.length - 1];
-
 // first API call to retrieve necessary information from current video
 var a1 = $.ajax({
-            url: 'https://www.giantbomb.com/api/video/' + current_video_id + '/',
+            url: 'https://www.giantbomb.com/api/video/' + getCurrentVideoId() + '/',
             dataType: 'json',
             data: { api_key: '5a510947131f62ca7c62a7ef136beccae13da2fd',
                     field_list: 'id,publish_date,video_show,video_categories',
@@ -19,9 +14,9 @@ var a1 = $.ajax({
                because some shows and categories have >100 videos, which is the
                upper limit of how many can be returned in a search.
             */
-            var publish_date = moment(data.results.publish_date, 'YYYY-MM-DD hh:mm:ss');
-            var start_date   = publish_date.clone().subtract(3, 'months');
-            var end_date     = publish_date.clone().add(3, 'months');
+            var publish_date = moment(data.results.publish_date, 'YYYY-MM-DD hh:mm:ss'),
+                start_date   = publish_date.clone().subtract(3, 'months'),
+                end_date     = publish_date.clone().add(3, 'months');
 
             var f1 = [
                 'publish_date:',
@@ -31,9 +26,9 @@ var a1 = $.ajax({
               ].join('');
 
             // filters search by the video's show/category
-            var video_show       = data.results.video_show;
-            var video_categories = data.results.video_categories;
-            var f2 = getVideoFilter(video_show, video_categories);
+            var video_show       = data.results.video_show,
+                video_categories = data.results.video_categories,
+                f2 = getVideoFilter(video_show, video_categories);
 
             if (f2 != null) {
               // second API call to get info on other videos of same show/category.
@@ -51,11 +46,12 @@ var a1 = $.ajax({
 
 // once API calls are completed, create and inject the HTML
 a2.done(function(data) {
-  var indices = [];
+  var current_video_id = getCurrentVideoId().split('-')[1],
+      indices = [];
 
   // results come in reverse-chronological order, so i = 0 is latest video
   for (var i = 0; i < data.results.length; i++) {
-    if (data.results[i].id != current_video_id.split('-')[1]) continue;
+    if (data.results[i].id != current_video_id) continue;
 
     if (i == 0) {
       indices[0] = 1;
@@ -77,10 +73,10 @@ a2.done(function(data) {
   var html = ['<div id="qol_prev_vid">'];
 
   if (indices[0] != null) {
-    var prev_video_image = data.results[indices[0]].image.thumb_url;
-    var prev_video_name  = data.results[indices[0]].name;
-    var prev_video_url   = data.results[indices[0]].site_detail_url;
-    var prev_arrow       = browser.extension.getURL('img/prev.png');
+    var prev_video_image = data.results[indices[0]].image.thumb_url,
+        prev_video_name  = data.results[indices[0]].name,
+        prev_video_url   = data.results[indices[0]].site_detail_url,
+        prev_arrow       = browser.extension.getURL('img/prev.png');
 
     html.push(
       '<a id="qol_prev_vid_link" href="' + prev_video_url + '">',
@@ -96,10 +92,10 @@ a2.done(function(data) {
   );
 
   if (indices[1] != null) {
-    var next_video_image = data.results[indices[1]].image.thumb_url;
-    var next_video_name  = data.results[indices[1]].name;
-    var next_video_url   = data.results[indices[1]].site_detail_url;
-    var next_arrow       = browser.extension.getURL("img/next.png");
+    var next_video_image = data.results[indices[1]].image.thumb_url,
+        next_video_name  = data.results[indices[1]].name,
+        next_video_url   = data.results[indices[1]].site_detail_url,
+        next_arrow       = browser.extension.getURL("img/next.png");
 
     html.push(
       '<a id="qol_next_vid_link" href="' + next_video_url + '">',
@@ -120,6 +116,14 @@ a2.done(function(data) {
   var parentElement = document.getElementsByClassName('tab-content')[0];
   parentElement.insertBefore(div, parentElement.firstChild);
 });
+
+// Trawls the web page for the video ID. Twitter link should be safe.
+function getCurrentVideoId() {
+  var attr = document.getElementsByClassName('share-twitter')[0]
+                      .getAttribute('data-event-tracking').split('|');
+
+  return attr[attr.length - 1];
+}
 
 /* Determines which show/category filter to use. Prioritizes show over category,
    and selects the first category in the list if there are multiple.
