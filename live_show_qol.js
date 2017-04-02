a141398137e2a73cb475c6212b7596041c3f49ef
Lowell Kissling <lrkissling@gmail.com>
2017-04-02 00:09:57 -0700

    changes to live_show_qol.js

--- live_show_qol.js
+++ live_show_qol.js
@@ -1,32 +1,36 @@
 console.log("live_show_qol loading");
 
+$(document).ready(function() {
+  $("#qol_show_emoji").click(function() {
+    document.getElementById("qol_emojis").classList.toggle("show");
+  });
+
+  $("#qol_blah").click(function() {
+    $("#f_ChatInput").val($("#f_ChatInput").val() + this.value);
+  });
+});
+
 var anime_bomb = browser.extension.getURL("img/anime-bomb.png");
 var bye = browser.extension.getURL("img/bye.png");
 var html = [
     "<div class='qol-dropdown'>",
-    "<button id='qol_show_emoji' title='Emojis' type='button' class='buttonEnabled'>",
+    "<a id='qol_show_emoji' title='Emojis'>",
     "<img id='qol_anime_bomb' src='" + anime_bomb + "'>",
-    "</button>",
+    "</a>",
     "<div id='qol_emojis' class='qol-dropdown-content'>",
     "<button id='qol_blah' class='qol-emoji' value=':bye'>",
-    "<img src='" + bye + "' title=':bye'>",
+    "<img src='" + bye + "' title=':bye '>",
     "</button></div></div>"
 ].join("");
 
 var span = document.createElement("span");
 span.innerHTML = html;
 
-var parentElement = document.getElementsByClassName("toolbar")[0];
-parentElement.insertBefore(span, parentElement.children[4]);
+// var parentElement = document.getElementsByClassName("toolbar")[0];
+// parentElement.insertBefore(span, parentElement.children[4]);
 
-$("#qol_show_emoji").click(function() {
-  document.getElementById("qol_emojis").classList.toggle("show");
-});
-
-// only works in chat
-$("#qol_blah").click(function() {
-  $("#f_ChatInput").val($("#f_ChatInput").val() + this.value);
-});
+var parentElement = $(".chat-panel__header")[0];
+parentElement.appendChild(span);
 
 // Close dropdown in user clicks outside of it.
 window.onclick = function(event) {
