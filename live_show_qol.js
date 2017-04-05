25635dde4beb364e7c8d40ce288f6accc411f6e6
Lowell Kissling <lrkissling@gmail.com>
2017-04-05 02:26:58 -0700

    first complete pass at adding selectable emojis to live chat

--- live_show_qol.js
+++ live_show_qol.js
@@ -1,63 +1,58 @@
-console.log("live_show_qol loading");
-
 $(document).ready(function() {
+  // toggle classes to display emoji and show button as active
   $("#qol_show_emoji").click(function() {
     $(this).toggleClass("qol-red-background");
-    $("#qol_emojis").toggleClass("qol-show");
+    $("#conversation-main").toggleClass("qol-height-60");
+    $("#qol_conversation_emoji").toggleClass("qol-show");
     return false;
   });
 
-  $("#qol_blah").click(function() {
+  // populate the chat input with the emoji key
+  $(".qol-emoji").click(function() {
     $("#f_ChatInput").val($("#f_ChatInput").val() + this.value);
   });
 });
 
+// html for the Emojis tab
 var hardcore = browser.extension.getURL("img/emojis/hardcore.png"),
-    bye      = browser.extension.getURL("img/emojis/bye.png");
-    html     = [
+    tab_html = [
       "<a id='qol_show_emoji' class='chat-tabs__wrapper' href='#' rel='nofollow'>",
       "<span class='chat-tabs__label'>",
-      "<img id='qol_anime_bomb' class='icon' src='" + hardcore + "'/>",
-      "Emojis</span></a>",
-      "<div id='qol_emojis' class='qol-dropdown-content'>"
-    ];
+      "<img id='qol_emoji_icon' class='icon' src='" + hardcore + "'/>",
+      "Emojis</span></a>"
+    ].join("");
 
+var li = document.createElement("li");
+li.className = "qol-chat-tabs__emoji";
+li.innerHTML = tab_html;
+
+var parentElement = $("#chatTabs")[0];
+parentElement.appendChild(li);
+
+// parse the emojis.json to create html for the emojis list
 $.getJSON(browser.extension.getURL("emojis.json"), function(data) {
+  let emojis_html = [];
+
   data.emojis.forEach(function(emoji) {
-    let src   = browser.extension.getURL(emoji.img),
+    let src  = browser.extension.getURL(emoji.img),
         name = emoji.name;
 
-    html.push(
-      "<button id='qol_blah' class='qol-emoji' value='" + name + " '>",
+    emojis_html.push(
+      "<button class='qol-emoji' value='" + name + " '>",
       "<img src='" + src + "' title='" + name + "'/></button>",
     );
   });
 
-  html.push("</div>");
-  html = html.join("");
+  emojis_html = emojis_html.join("");
 
-  let li = document.createElement("li");
-  li.className = "qol-dropdown";
-  li.innerHTML = html;
+  let div = document.createElement("div");
+  div.id = "qol_conversation_emoji";
+  div.className = "chat-panel";
+  div.innerHTML = emojis_html;
 
-  let parentElement = $("#chatTabs")[0];
-  parentElement.appendChild(li);
+  let parentElement = $("#chat-canvas")[0];
+  parentElement.appendChild(div);
 });
 
 // var parentElement = document.getElementsByClassName("toolbar")[0];
 // parentElement.insertBefore(span, parentElement.children[4]);
-
-// Close dropdown in user clicks outside of it.
-window.onclick = function(event) {
-  if (!event.target.matches("#qol_show_emoji")       &&
-      !event.target.matches(".qol-dropdown-content") &&
-      !event.target.matches(".qol-emoji")) {
-    let dropdowns = document.getElementsByClassName("qol-dropdown-content");
-    for (var i = 0, len = dropdowns.length; i < len; i++) {
-      let openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains("show")) {
-        openDropdown.classList.remove("show");
-      }
-    }
-  }
-};
