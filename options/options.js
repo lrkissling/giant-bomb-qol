16c86cec9397faf0d2a70f70bd906d842821676d
Lowell Kissling <lrkissling@gmail.com>
2017-04-10 21:00:56 -0700

    Added API Key and Chat Emoji options. Still not functional

--- options/options.js
+++ options/options.js
@@ -4,22 +4,58 @@
 function saveOptions(e) {
   e.preventDefault();
   browser.storage.local.set({
-    prev_next_vids: document.querySelector("#cbox-prev-next-vids").checked,
+    api_key: document.querySelector("#text_api_key").value.trim(),
+    prev_next_vids: document.querySelector("#cbox_prev_next_vids").checked,
+    chat_emoji: document.querySelector("#cbox_chat_emoji").checked
   });
 }
 
-// Invoke saveOptions whenever a checkbox is changed
-document.querySelector("#cbox-prev-next-vids").addEventListener("change", saveOptions);
+// Handle mouseover/mouseout of all infobuttons
+var elems = document.getElementsByClassName("option-infobutton-container");
+Array.from(elems).forEach(function(e) {
+  e.addEventListener("mouseover",function() {
+    Array.from(this.getElementsByClassName("option-infobutton-text")).forEach(function(t) {
+      t.style.display = "block";
+    });
+  });
+  e.addEventListener("mouseout", function() {
+    Array.from(this.getElementsByClassName("option-infobutton-text")).forEach(function(t) {
+      t.style.display = "none";
+    });
+  });
+});
+
+// Invoke saveOptions whenever an option is changed
+document.querySelector("#text_api_key").addEventListener("change", saveOptions);
+document.querySelector("#cbox_prev_next_vids").addEventListener("change", saveOptions);
+document.querySelector("#cbox_chat_emoji").addEventListener("change", saveOptions);
 
 function restoreOptions() {
   /**
-  * Set the checkbox according to user option, default to checked if null
+  * Set the API Key according to user option, default to empty if null
+  */
+  function setApiKey(result) {
+    if (result.api_key) {
+      document.querySelector("#text_api_key").value = result.api_key;
+    }
+  }
+
+  /**
+  * Set the following checkboxes according to user option, default to checked if null
   */
   function setPrevNextVids(result) {
     if (result.prev_next_vids != null) {
-      document.querySelector("#cbox-prev-next-vids").checked = result.prev_next_vids;
+      document.querySelector("#cbox_prev_next_vids").checked = result.prev_next_vids;
     } else {
-      document.querySelector("#cbox-prev-next-vids").checked = true;
+      document.querySelector("#cbox_prev_next_vids").checked = true;
+    }
+  }
+
+  function setChatEmoji(result) {
+    if (result.chat_emoji != null) {
+      document.querySelector("#cbox_chat_emoji").checked = result.chat_emoji;
+    } else {
+      document.querySelector("#cbox_chat_emoji").checked = true;
     }
   }
 
@@ -28,8 +64,14 @@ function restoreOptions() {
   }
 
   // Retrieve promises of options from local storage and send to handler functions
-  var getting = browser.storage.local.get("prev_next_vids");
+  let getting = browser.storage.local.get("api_key");
+  getting.then(setApiKey, onError);
+
+  getting = browser.storage.local.get("prev_next_vids");
   getting.then(setPrevNextVids, onError);
+
+  getting = browser.storage.local.get("chat_emoji");
+  getting.then(setChatEmoji, onError);
 }
 
 // Populate the options page with stored settings after DOM is loaded
