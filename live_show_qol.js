16cc198bc1ac8e8fbfc276f3abee8b139be1e250
Lowell Kissling <lrkissling@gmail.com>
2017-04-12 19:03:43 -0700

    integrated chrome-compatible options handling for live_show_qol and video_navigator

--- live_show_qol.js
+++ live_show_qol.js
@@ -1,57 +1,66 @@
-$(document).ready(function() {
-  // toggle classes to display emoji and show button as active
-  $("#qol_show_emoji").click(function() {
-    $(this).toggleClass("qol-red-background");
-    $("#conversation-main").toggleClass("qol-height-60");
-    $("#qol_conversation_emoji").toggleClass("qol-show");
-    return false;
-  });
+// Only load the buttons if the user option is true. Defaults to true.
+if (navigator.userAgent.indexOf("Chrome") != -1) {
+  chrome.storage.sync.get("chat_emoji", handleOptions);
+}
 
-  // populate the chat input with the emoji key
-  $(".qol-emoji").click(function() {
-    $("#f_ChatInput").val($("#f_ChatInput").val() + this.value);
-    $("#f_ChatInput").focus();
+// Check that they want the prev and next vids, and use their API key if valid.
+function handleOptions(item) {
+  if (items.chat_emoji === undefined || items.chat_emoji) {
+    createEmojiMenu();
+  }
+}
+
+function createEmojiMenu() {
+  // html for the Emojis tab
+  let hardcore = chrome.extension.getURL("img/emojis/hardcore.png"),
+      tab_html = [
+        "<a id='qol_show_emoji' class='chat-tabs__wrapper' href='#' rel='nofollow'>",
+        "<span class='chat-tabs__label'>",
+        "<img id='qol_emoji_icon' class='icon' src='" + hardcore + "'/>",
+        "Emojis</span></a>"
+      ].join("");
+
+  let li = document.createElement("li");
+  li.className = "qol-chat-tabs__emoji";
+  li.innerHTML = tab_html;
+
+  let parentElement = $("#chatTabs")[0];
+  parentElement.appendChild(li);
+
+  // parse the emojis.json to create html for the emojis list
+  $.getJSON(chrome.extension.getURL("emojis.json"), function(data) {
+    let emojis_html = [];
+
+    data.emojis.forEach(function(emoji) {
+      let src  = chrome.extension.getURL(emoji.img),
+          name = emoji.name;
+
+      emojis_html.push("<button class='qol-emoji' value='" + name + " '>");
+      emojis_html.push("<img src='" + src + "' title='" + name + "'/></button>");
+    });
+
+    emojis_html = emojis_html.join("");
+
+    let div = document.createElement("div");
+    div.id = "qol_conversation_emoji";
+    div.className = "chat-panel";
+    div.innerHTML = emojis_html;
+
+    let parentElement = $("#chat-canvas")[0];
+    parentElement.appendChild(div);
   });
+}
+
+// toggle classes to display emoji and show button as active
+$("#qol_show_emoji").click(function() {
+  $(this).toggleClass("qol-red-background");
+  $("#conversation-main").toggleClass("qol-height-60");
+  $("#qol_conversation_emoji").toggleClass("qol-show");
+  return false;
 });
 
-// html for the Emojis tab
-var hardcore = chrome.extension.getURL("img/emojis/hardcore.png"),
-    tab_html = [
-      "<a id='qol_show_emoji' class='chat-tabs__wrapper' href='#' rel='nofollow'>",
-      "<span class='chat-tabs__label'>",
-      "<img id='qol_emoji_icon' class='icon' src='" + hardcore + "'/>",
-      "Emojis</span></a>"
-    ].join("");
-
-var li = document.createElement("li");
-li.className = "qol-chat-tabs__emoji";
-li.innerHTML = tab_html;
-
-var parentElement = $("#chatTabs")[0];
-parentElement.appendChild(li);
-
-// parse the emojis.json to create html for the emojis list
-$.getJSON(chrome.extension.getURL("emojis.json"), function(data) {
-  let emojis_html = [];
-
-  data.emojis.forEach(function(emoji) {
-    let src  = chrome.extension.getURL(emoji.img),
-        name = emoji.name;
-
-    emojis_html.push("<button class='qol-emoji' value='" + name + " '>");
-    emojis_html.push("<img src='" + src + "' title='" + name + "'/></button>");
-  });
-
-  emojis_html = emojis_html.join("");
-
-  let div = document.createElement("div");
-  div.id = "qol_conversation_emoji";
-  div.className = "chat-panel";
-  div.innerHTML = emojis_html;
-
-  let parentElement = $("#chat-canvas")[0];
-  parentElement.appendChild(div);
+// populate the chat input with the emoji key
+$(".qol-emoji").click(function() {
+  $("#f_ChatInput").val($("#f_ChatInput").val() + this.value);
+  $("#f_ChatInput").focus();
 });
-
-// var parentElement = document.getElementsByClassName("toolbar")[0];
-// parentElement.insertBefore(span, parentElement.children[4]);
--- video_navigator.js
+++ video_navigator.js
@@ -1,128 +1,146 @@
 console.log("giant-bomb-qol loading");
 
-// first API call to retrieve necessary information from current video
-var a1 = $.ajax({
-            url: "https://www.giantbomb.com/api/video/" + getCurrentVideoId() + "/",
-            dataType: "json",
-            data: { api_key: "5a510947131f62ca7c62a7ef136beccae13da2fd",
-                    field_list: "publish_date,video_show,video_categories",
-                    format: "json"
-                  }
-         }),
-    a2 = a1.then(function(data) {
-            /* Narrows the search to within 3 months of current video. Necessary
-               because some shows and categories have >100 videos, which is the
-               upper limit of how many can be returned in a search.
-            */
-            var date_format  = "YYYY-MM-DD hh:mm:ss";
-                publish_date = moment(data.results.publish_date, date_format),
-                start_date   = publish_date.clone().subtract(3, "months"),
-                end_date     = publish_date.clone().add(3, "months");
+// Only load the buttons if the user option is true. Defaults to true.
+if (navigator.userAgent.indexOf("Chrome") != -1) {
+  chrome.storage.sync.get(["api_key","prev_next_vids"], handleOptions);
+}
 
-            var f1 = [
-                "publish_date:",
-                start_date.format(date_format),
-                "|",
-                end_date.format(date_format)
-              ].join("");
-
-            // filters search by the video's show/category
-            var video_show       = data.results.video_show,
-                video_categories = data.results.video_categories,
-                f2 = getVideoFilter(video_show, video_categories);
-
-            if (f2 !== null) {
-              // second API call to get info on other videos of same show/category.
-              return $.ajax({
-                url: "https://www.giantbomb.com/api/videos/",
-                dataType: "json",
-                data: { api_key: "5a510947131f62ca7c62a7ef136beccae13da2fd",
-                        field_list: "id,image,name,publish_date,site_detail_url",
-                        filter: f1 + "," + f2,
-                        format: "json"
-                      }
-              });
-            }
-         });
-
-// once API calls are completed, create and inject the HTML
-a2.done(function(data) {
-  var current_video_id = getCurrentVideoId().split("-")[1],
-      indices = [];
-
-  // results come in reverse-chronological order, so i = 0 is latest video
-  for (var i = 0, len = data.results.length; i < len; i++) {
-    if (data.results[i].id != current_video_id) continue;
-
-    if (i === 0) {
-      indices[0] = 1;
+function handleOptions(item) {
+  if (items.prev_next_vids === undefined || items.prev_next_vids) {
+    let api_key = items.api_key;
+    if (api_key === undefined || api_key.length !== 40) {
+      // default api key
+      api_key = "5a510947131f62ca7c62a7ef136beccae13da2fd";
     }
-    else if (i === len - 1) {
-      indices[1] = i - 1;
-    }
-    else {
-      indices[0] = i + 1;
-      indices[1] = i - 1;
-    }
-
-    break;
+    showPrevAndNexVids(api_key);
   }
+}
 
-  if (indices.length === 0) return;
+function showPrevAndNexVids(api_key) {
+  // first API call to retrieve necessary information from current video
+  let a1 = $.ajax({
+              url: "https://www.giantbomb.com/api/video/" + getCurrentVideoId() + "/",
+              dataType: "json",
+              data: { api_key: api_key,
+                      field_list: "publish_date,video_show,video_categories",
+                      format: "json"
+                    }
+           }),
+      a2 = a1.then(function(data) {
+              /*
+                 Narrows the search to within 3 months of current video. Necessary
+                 because some shows and categories have >100 videos, which is the
+                 upper limit of how many can be returned in a search.
+              */
+              let date_format  = "YYYY-MM-DD hh:mm:ss",
+                  publish_date = moment(data.results.publish_date, date_format),
+                  start_date   = publish_date.clone().subtract(3, "months"),
+                  end_date     = publish_date.clone().add(3, "months");
 
-  // start building the actual html
-  var html = ["<div id='qol_prev_vid'>"];
+              let f1 = [
+                  "publish_date:",
+                  start_date.format(date_format),
+                  "|",
+                  end_date.format(date_format)
+                ].join("");
 
-  if (indices[0] !== undefined) {
-    var prev_video_image = data.results[indices[0]].image.thumb_url,
-        prev_video_name  = data.results[indices[0]].name,
-        prev_video_url   = data.results[indices[0]].site_detail_url,
-        prev_arrow       = chrome.extension.getURL("img/prev.png");
+              // filters search by the video's show/category
+              let video_show       = data.results.video_show,
+                  video_categories = data.results.video_categories,
+                  f2 = getVideoFilter(video_show, video_categories);
+
+              if (f2 !== null) {
+                // second API call to get info on other videos of same show/category.
+                return $.ajax({
+                  url: "https://www.giantbomb.com/api/videos/",
+                  dataType: "json",
+                  data: { api_key: api_key,
+                          field_list: "id,image,name,publish_date,site_detail_url",
+                          filter: f1 + "," + f2,
+                          format: "json"
+                        }
+                });
+              }
+           });
+
+  // once API calls are completed, create and inject the HTML
+  a2.done(function(data) {
+    let current_video_id = getCurrentVideoId().split("-")[1],
+        indices = [];
+
+    // results come in reverse-chronological order, so i = 0 is latest video
+    for (var i = 0, len = data.results.length; i < len; i++) {
+      if (data.results[i].id != current_video_id) continue;
+
+      if (i === 0) {
+        indices[0] = 1;
+      }
+      else if (i === len - 1) {
+        indices[1] = i - 1;
+      }
+      else {
+        indices[0] = i + 1;
+        indices[1] = i - 1;
+      }
+
+      break;
+    }
+
+    if (indices.length === 0) return;
+
+    // start building the actual html
+    let html = ["<div id='qol_prev_vid'>"];
+
+    if (indices[0] !== undefined) {
+      let prev_video_image = data.results[indices[0]].image.thumb_url,
+          prev_video_name  = data.results[indices[0]].name,
+          prev_video_url   = data.results[indices[0]].site_detail_url,
+          prev_arrow       = chrome.extension.getURL("img/prev.png");
+
+      html.push(
+        "<a id='qol_prev_vid_link' href='" + prev_video_url + "'>",
+        "<img id ='qol_prev_vid_arrow' src='" + prev_arrow + "'/>",
+        "<span class='qol-vid-name'>" + prev_video_name + "</span>",
+        "<img id='qol_prev_vid_thumb' class='qol_thumb' src='" + prev_video_image + "'></a>"
+      );
+    }
 
     html.push(
-      "<a id='qol_prev_vid_link' href='" + prev_video_url + "'>",
-      "<img id ='qol_prev_vid_arrow' src='" + prev_arrow + "'/>",
-      "<span class='qol-vid-name'>" + prev_video_name + "</span>",
-      "<img id='qol_prev_vid_thumb' class='qol_thumb' src='" + prev_video_image + "'></a>"
+      "</div>",
+      "<div id='qol_next_vid'>"
     );
-  }
 
-  html.push(
-    "</div>",
-    "<div id='qol_next_vid'>"
-  );
+    if (indices[1] !== undefined) {
+      let next_video_image = data.results[indices[1]].image.thumb_url,
+          next_video_name  = data.results[indices[1]].name,
+          next_video_url   = data.results[indices[1]].site_detail_url,
+          next_arrow       = chrome.extension.getURL("img/next.png");
 
-  if (indices[1] !== undefined) {
-    var next_video_image = data.results[indices[1]].image.thumb_url,
-        next_video_name  = data.results[indices[1]].name,
-        next_video_url   = data.results[indices[1]].site_detail_url,
-        next_arrow       = chrome.extension.getURL("img/next.png");
+      html.push(
+        "<a id='qol_next_vid_link' href='" + next_video_url + "'>",
+        "<img id='qol_next_vid_thumb' class='qol_thumb' src='" + next_video_image + "'>",
+        "<span class='qol-vid-name'>" + next_video_name + "</span>",
+        "<img id='qol_next_vid_arrow' src='" + next_arrow + "'/></a>"
+      );
+    }
 
-    html.push(
-      "<a id='qol_next_vid_link' href='" + next_video_url + "'>",
-      "<img id='qol_next_vid_thumb' class='qol_thumb' src='" + next_video_image + "'>",
-      "<span class='qol-vid-name'>" + next_video_name + "</span>",
-      "<img id='qol_next_vid_arrow' src='" + next_arrow + "'/></a>"
-    );
-  }
+    html.push("</div>");
+    html = html.join("");
 
-  html.push("</div>");
-  html = html.join("");
+    let div = document.createElement("div");
+    div.setAttribute("id", "qol_video_navigator");
+    div.setAttribute("class", "tab-pane");
+    div.innerHTML = html;
 
-  var div = document.createElement("div");
-  div.setAttribute("id", "qol_video_navigator");
-  div.setAttribute("class", "tab-pane");
-  div.innerHTML = html;
-
-  var parentElement = document.getElementsByClassName("tab-content")[0];
-  parentElement.insertBefore(div, parentElement.firstChild);
-});
+    let parentElement = document.getElementsByClassName("tab-content")[0];
+    parentElement.insertBefore(div, parentElement.firstChild);
+  });
+}
 
 // Trawls the web page for the video ID. Twitter link should be safe.
 function getCurrentVideoId() {
-  var attr = document.getElementsByClassName("share-twitter")[0]
-                      .getAttribute("data-event-tracking").split("|");
-
+  let attr = document.getElementsByClassName("share-twitter")[0]
+                     .getAttribute("data-event-tracking").split("|");
   return attr[attr.length - 1];
 }
 
