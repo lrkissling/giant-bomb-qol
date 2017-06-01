e5b18b57629a742f7569e31ffd0c8c6d720ea583
Lowell Kissling <lrkissling@gmail.com>
2017-06-01 01:48:53 -0700

    removed TITR spoilers from video thumbnails

--- hide_spoilers.js
+++ hide_spoilers.js
@@ -1,43 +1,45 @@
-// options currently only work for Chrome
 if (navigator.userAgent.indexOf("Chrome") != -1) {
   chrome.storage.sync.get("hide_titr_spoilers", handleOptions);
 } else {
-  getting = browser.storage.sync.get("hide_titr_spoilers");
-  getting.then(handleOptions, onError);
-}
-
-// Check that they want to hide spoilers
-function handleOptions(items) {
-  if (items.hide_titr_spoilers === undefined || items.hide_titr_spoilers) {
-    if (!isMainPage(window.location.href) ||
-        document.querySelector("h2").innerHTML.includes("This Is The Run")) {
-      hideSpoilerElements();
-    }
-  }
-}
-
-function isMainPage(url) {
-  if (url === "https://www.giantbomb.com/" ||
-      url === "http://www.giantbomb.com/") {
-    return true;
-  }
-  return false;
+  browser.storage.sync.get("hide_titr_spoilers").then(handleOptions, onError);
 }
 
 function onError(error) {
   console.log(`Error: ${error}`);
 }
 
-function hideSpoilerElements() {
-  let div = document.getElementsByClassName("av-buffer")[0];
-  div.classList.add("qol-hidden");
+// Check that they want to hide spoilers
+function handleOptions(items) {
+  if (items.hide_titr_spoilers === undefined || items.hide_titr_spoilers) {
+    // Ensure that the video is a TITR video
+    if ($("#site-main h2").first().text().toUpperCase().includes("THIS IS THE RUN")) {
+      hideVideoSpoilers();
+    }
 
-  div = document.getElementsByClassName("js-vid-buffer av-buffer-wrap")[0];
-  div.classList.add("qol-hidden");
+    hideThumbnailSpoilers();
+  }
+}
 
-  div = document.getElementsByClassName("js-vid-position av-position")[0];
-  div.classList.add("qol-hidden");
+// Hides the timestamp and progress buffer of the video.
+function hideVideoSpoilers() {
+  $(".av-buffer").first().addClass("qol-hidden");
+  $(".js-vid-buffer.av-buffer-wrap").first().addClass("qol-hidden");
+  $(".js-vid-position.av-position").first().addClass("qol-hidden");
+  $(".av-time").first().addClass("qol-hidden");
+}
 
-  div = document.getElementsByClassName("av-time")[0];
-  div.classList.add("qol-hidden");
+// Hides timestamps from video thumbnails
+function hideThumbnailSpoilers() {
+  // if it's the TITR show page, hide every thumbnail timestamp
+  if (window.location.href.includes("giantbomb.com/shows/this-is-the-run/")) {
+    $(".overlay-stats i:last-child").css("display", "none");
+  }
+  // otherwise, only hide timestamps from thumbnails for TITR videos
+  else {
+    $("#video-block li a").each(function() {
+      if (this.href.includes("this-is-the-run")) {
+        $(this).find(".overlay-stats i:last-child").css("display", "none");
+      }
+    });
+  }
 }
