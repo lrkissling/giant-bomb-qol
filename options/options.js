7c4c12464ce0bc50b6a515ea2fb6b58d6f9e1b35
Lowell Kissling <lrkissling@gmail.com>
2019-01-05 00:12:21 -0800

    remove video_navigator. i'll miss ya, buddy.

--- options/options.js
+++ options/options.js
@@ -7,14 +7,12 @@ if (navigator.userAgent.indexOf("Chrome") != -1) {
 
 var apiKey              = document.querySelector("#text_api_key"),
     streamNotifications = document.querySelector("#cbox_stream_notifications"),
-    prevNextVids        = document.querySelector("#cbox_prev_next_vids"),
     hideTitrSpoilers    = document.querySelector("#cbox_hide_titr_spoilers"),
     chatEmotes          = document.querySelector("#cbox_chat_emotes");
 
 // Invoke saveOptions whenever an option is changed
 $(apiKey).on("input", saveOptions);
 $(streamNotifications).on("change", saveOptions);
-$(prevNextVids).on("change", saveOptions);
 $(hideTitrSpoilers).on("change", saveOptions);
 $(chatEmotes).on("change", saveOptions);
 
@@ -41,7 +39,6 @@ function saveOptions(e) {
   let options = {
     api_key: apiKey.value.trim(),
     stream_notifications: streamNotifications.checked,
-    prev_next_vids: prevNextVids.checked,
     hide_titr_spoilers: hideTitrSpoilers.checked,
     chat_emotes: chatEmotes.checked
   };
@@ -91,15 +88,6 @@ function restoreOptions() {
     streamNotifications.disabled = !hasValidKey();
   }
 
-  // Set the Prev/Next Vids checkbox according to user option, default to checked if null
-  function setPrevNextVids(result) {
-    if (result.prev_next_vids !== undefined) {
-      prevNextVids.checked = result.prev_next_vids;
-    } else {
-      prevNextVids.checked = true;
-    }
-  }
-
   // Set the Hide TITR Spoilers checkbox according to user option, default to checked if null
   function setHideTitrSpoilers(result) {
     if (result.hide_titr_spoilers !== undefined) {
@@ -122,13 +110,11 @@ function restoreOptions() {
   if (navigator.userAgent.indexOf("Chrome") != -1) {
     chrome.storage.sync.get("api_key", setApiKey);
     chrome.storage.sync.get("stream_notifications", setStreamNotifications);
-    chrome.storage.sync.get("prev_next_vids", setPrevNextVids);
     chrome.storage.sync.get("hide_titr_spoilers", setHideTitrSpoilers);
     chrome.storage.sync.get("chat_emotes", setChatEmotes);
   } else {
     browser.storage.sync.get("api_key").then(setApiKey, onError);
     browser.storage.sync.get("stream_notifications").then(setStreamNotifications, onError);
-    browser.storage.sync.get("prev_next_vids").then(setPrevNextVids, onError);
     browser.storage.sync.get("hide_titr_spoilers").then(setHideTitrSpoilers, onError);
     browser.storage.sync.get("chat_emotes").then(setChatEmotes, onError);
   }
