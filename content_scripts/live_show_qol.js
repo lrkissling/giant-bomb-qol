f499435e917f0d8bdd81be3a3d4a5a18344495a9
Lowell Kissling <lrkissling@gmail.com>
2020-04-28 21:04:43 -0700

    support for emotes menu on pages that aren't /chat and /infinite

--- content_scripts/live_show_qol.js
+++ content_scripts/live_show_qol.js
@@ -1,5 +1,7 @@
+var show_emotes_menu = false;
 var show_infobuttons = false;
 
+// handle options, using browser-specific option retrieval.
 if (navigator.userAgent.indexOf("Chrome") != -1) {
   chrome.storage.sync.get(["chat_emotes", "infinite_infobuttons"], handleOptions);
 } else {
@@ -7,16 +9,14 @@ if (navigator.userAgent.indexOf("Chrome") != -1) {
   getting.then(handleOptions, onError);
 }
 
-// Check that they want the Emotes Menu
+// Check user options to see if they want the emotes menu or infobuttons
 function handleOptions(item) {
   if (item.chat_emotes === undefined || item.chat_emotes) {
-    createEmotesMenu();
+    show_emotes_menu = true;
   }
 
   if (item.infinite_infobuttons === undefined || item.infinite_infobuttons) {
     show_infobuttons = true;
-  } else {
-    show_infobuttons = false;
   }
 }
 
@@ -24,6 +24,51 @@ function onError(error) {
   console.log(`Error: ${error}`);
 }
 
+$(document).ready(function() {
+  if (document.getElementById('js-chat-container')) {
+    if (show_emotes_menu) {
+      emoteSetup();
+    }
+
+    if (show_infobuttons && window.location.href.indexOf("infinite") > -1) {
+      infobuttonSetup();
+    }
+  }
+});
+
+function emoteSetup() {
+  createEmotesMenu();
+
+  // toggle classes to display emotes and show button as active
+  $("#chatTabs").on("click", "#qol_show_emotes", function() {
+    $(this).toggleClass("qol-red-background");
+    $("#conversation-main").toggleClass("qol-height-60");
+    $("#js-main-chat").toggleClass("qol-height-60");
+    $("#qol_conversation_emotes").toggleClass("qol-show");
+    return false;
+  });
+
+  // populate the chat input with the emote key
+  $("#chat-canvas").on("click", ".qol-emote", function() {
+    $("#f_ChatInput").val($("#f_ChatInput").val() + this.value);
+    $("#f_ChatInput").focus();
+  });
+
+  // Sets up New Emotes functionality, only works for Firefox
+  if (navigator.userAgent.indexOf("Chrome") == -1) {
+    // build single list of emotes already in the extension
+    let old_emotes = [];
+    $.getJSON(chrome.extension.getURL("resources/emotes.json"), function(data) {
+      for (const category of Object.values(data)) {
+        for (const emote of Object.values(category)) {
+          old_emotes.push(emote.name.substring(1));
+        }
+      }
+    });
+    // need a brief timeout to make sure firechat emotes are populated
+    setTimeout(setNewEmotes.bind(null, old_emotes), 10 * 1000);
+  }
+}
 
 function createEmotesMenu() {
   // html for the Emotes tab
@@ -117,14 +162,49 @@ function setNewEmotes(old_emotes) {
   }
 }
 
+function infobuttonSetup() {
+  // Add infobuttons to active poll on initial page load, if necessary
+  addInfobuttons();
+
+  // Set up mutation observer for new polls
+  const poll_container = document.getElementById('js-poll-answer-container');
+  const observer_config = { childList: true };
+  // Callback function to execute when mutations are observed
+  const callback = function(mutationsList, observer) {
+    for (let mutation of mutationsList) {
+      if (mutation.addedNodes.length > 0) {
+        addInfobuttons();
+      }
+    }
+  };
+
+  // Create an observer instance linked to the callback function
+  const observer = new MutationObserver(callback);
+  // Start observing the poll container for mutations
+  observer.observe(poll_container, observer_config);
+
+  // Have to stop propagation of click event because it counts as poll answer.
+  $("#js-poll-answer-container").on("click", ".qol-infobutton", function(event) {
+    event.stopImmediatePropagation();
+  });
+
+  // Don't show check mark when hovering over infobutton
+  $("#js-poll-answer-container").on({
+    mouseenter: function() {
+      $(this).closest("li").addClass("qol-without-after");
+    },
+    mouseleave: function() {
+      $(this).closest("li").removeClass("qol-without-after");
+    }
+  }, ".qol-infobutton");
+}
+
 // Adds an infobutton linking to the video on QL Crew
 function addInfobuttons() {
   const src = chrome.extension.getURL("img/info.png");
 
   // For each poll option that doesn't already have an infobutton, and isn't the Mystery Box
-  // $.each($(".poll-choices__item > span:not(:contains('Mystery Box!'))"), function(index, value) {
   $.each($(".poll-choices__item > span:not(:has(a)):not(:contains('Mystery Box!'))"), function(index, value) {
-  // $.each($(".poll-choices__label"), function(index, value) { // for testing purposes
     // build query string
     let choice = $(value);
     const query = buildQueryString(choice.text());
@@ -154,74 +234,3 @@ function buildQueryString(text) {
   // return URI-encoded string
   return encodeURI(search_text);
 }
-
-$(document).ready(function() {
-  // toggle classes to display emotes and show button as active
-  $("#chatTabs").on("click", "#qol_show_emotes", function() {
-    $(this).toggleClass("qol-red-background");
-    $("#conversation-main").toggleClass("qol-height-60");
-    $("#js-main-chat").toggleClass("qol-height-60");
-    $("#qol_conversation_emotes").toggleClass("qol-show");
-    return false;
-  });
-
-  // populate the chat input with the emote key
-  $("#chat-canvas").on("click", ".qol-emote", function() {
-    $("#f_ChatInput").val($("#f_ChatInput").val() + this.value);
-    $("#f_ChatInput").focus();
-  });
-
-  // New Emotes functionality only works for Firefox
-  if (navigator.userAgent.indexOf("Chrome") == -1) {
-    // build single list of emotes already in the extension
-    let old_emotes = [];
-    $.getJSON(chrome.extension.getURL("resources/emotes.json"), function(data) {
-      for (const category of Object.values(data)) {
-        for (const emote of Object.values(category)) {
-          old_emotes.push(emote.name.substring(1));
-        }
-      }
-    });
-    // need a brief timeout to make sure firechat emotes are populated
-    setTimeout(setNewEmotes.bind(null, old_emotes), 10 * 1000);
-  }
-
-  // Set up infobutton handling for GB Infinite polls
-  if (show_infobuttons && window.location.href.indexOf("infinite") > -1) {
-    // $("#js-chat-tab-poll").on("click", addInfobuttons);
-
-    // Add infobuttons to active poll on initial page load, if necessary
-    addInfobuttons();
-
-    // Set up mutation observer for new polls
-    const poll_container = document.getElementById('js-poll-answer-container');
-    const observer_config = { childList: true };
-    // Callback function to execute when mutations are observed
-    const callback = function(mutationsList, observer) {
-      for (let mutation of mutationsList) {
-        if (mutation.addedNodes.length > 0) {
-          addInfobuttons();
-        }
-      }
-    };
-    // Create an observer instance linked to the callback function
-    const observer = new MutationObserver(callback);
-    // Start observing the poll container for mutations
-    observer.observe(poll_container, observer_config);
-
-    // Have to stop propagation of click event because it counts as poll answer.
-    $("#js-poll-answer-container").on("click", ".qol-infobutton", function(event) {
-      event.stopImmediatePropagation();
-    });
-
-    // Don't show check mark when hovering over infobutton
-    $("#js-poll-answer-container").on({
-      mouseenter: function() {
-        $(this).closest("li").addClass("qol-without-after");
-      },
-      mouseleave: function() {
-        $(this).closest("li").removeClass("qol-without-after");
-      }
-    }, ".qol-infobutton");
-  }
-});
