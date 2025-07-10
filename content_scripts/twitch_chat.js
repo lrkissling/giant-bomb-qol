7e3757355fb49fc6354f3f2c61e126ff941701f9
Lowell Kissling <lrkissling@gmail.com>
2025-07-09 22:17:35 -0700

    added poll sound option functionality to twitch_chat.js and fixed loading sequencing

--- content_scripts/twitch_chat.js
+++ content_scripts/twitch_chat.js
@@ -3,6 +3,29 @@ if (navigator.userAgent.indexOf("Chrome") != -1) {
   browser = chrome;
 }
 
+let gb_chat_style = false;
+let gbforever_poll_sound = false;
+
+// handle options
+browser.storage.sync.get([
+  "gb_chat_style", "gbforever_poll_sound",
+]).then(handleOptions, onError);
+
+// Check user options to see if they want the emotes menu or infobuttons
+function handleOptions(item) {
+  if (item.gb_chat_style === undefined || item.gb_chat_style) {
+    gb_chat_style = true;
+  }
+
+  if (item.gbforever_poll_sound === undefined || item.gbforever_poll_sound) {
+    gbforever_poll_sound = true;
+  }
+}
+
+function onError(error) {
+  console.error(`Error: ${error}`);
+}
+
 // audio for new polls on GB Forever
 const gbForeverAudio = new Audio(browser.runtime.getURL("resources/ding.mp3"));
 
@@ -10,19 +33,23 @@ const isGiantBotMessage = node => node.innerText.startsWith('GiantBotForever');
 const isNewPollMessage = node => node.innerText.includes('Voting has started!');
 
 $(document).ready(function() {
-  setTimeout(setupChatFeatures, 1 * 2000);
+  setTimeout(moveChatInputToTop, 1 * 1000);
+  setTimeout(scrollChatToTop, 1.5 * 1000);
+  setTimeout(setupChatFeatures, 1.5 * 1000);
 });
 
-function setupChatFeatures() {
-  // scroll chat to top
-  const chatScrollElem = document.getElementsByClassName("simplebar-scroll-content")[0];
-  chatScrollElem.scrollTop = 0;
-
-  // move chat input to top
+function moveChatInputToTop() {
   const chatContentElem = document.getElementsByClassName("chat-room__content")[0];
   const chatInputElem = document.getElementsByClassName("chat-input")[0];
   chatContentElem.before(chatInputElem);
+}
 
+function scrollChatToTop() {
+  const chatScrollElem = document.getElementsByClassName("simplebar-scroll-content")[0];
+  chatScrollElem.scrollTop = 0;
+}
+
+function setupChatFeatures() {
   // setup mutation observer for new chat messages
   setupChatMutationObserver();
 
@@ -47,7 +74,7 @@ function setupChatMutationObserver() {
         if (isGiantBotMessage(node)) {
           highlightGiantBotMessage(node);
 
-          if(isNewPollMessage(node)) {
+          if(isNewPollMessage(node) && gbforever_poll_sound) {
             gbForeverAudio.play();
           }
         }
